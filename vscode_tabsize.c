ad94c65423e76b13377654ef12996d120442df6a
starter28 <skymoon28@nate.com>
2021-01-02 05:56:38 +0100

    1

---
+++ vscode_tabsize.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vscode_tabsize.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junsulee <junsulee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/02 05:54:42 by junsulee          #+#    #+#             */
+/*   Updated: 2021/01/02 05:56:15 by junsulee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	test_funtciont()
+{
+	int		a;
+	char	b;
+	long	c;
+	float	d;
+
+	if (1)
+		pritnf("test");
+}
+
+int		main()
+{
+	int		a;
+	char	b;
+	short	c;
+	float	d;
+
+	while (1)
+	{
+		printf("test");
+		if (1)
+			break ;
+	}
+}
